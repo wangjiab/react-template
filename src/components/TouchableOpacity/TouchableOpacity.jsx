431406d104cb2140703f2bd94811cc40536631be
wangjiab <864789126@qq.com>
2018-05-21 17:55:29 +0800

    react-模板

---
+++ src/components/TouchableOpacity/TouchableOpacity.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { is, fromJS } from 'immutable';
+import './TouchableOpacity.css';
+/**
+ * 点击状态组件
+ */
+export default class TouchableOpacity extends Component{
+  static propTypes={
+    className:PropTypes.string,
+    text:PropTypes.string,
+    clickCallBack:PropTypes.func
+  }
+  handleTouchStart = () => {
+    this.refs.btn.style.opacity = '0.3';
+  }
+
+  handleTouchEnd = () => {
+    this.refs.btn.style.opacity = '1';
+    this.props.clickCallBack();
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state), fromJS(nextState))
+  }
+  
+  render(){
+    return (
+      <div className={`btn-con ${this.props.className}`} onTouchStart={this.handleTouchStart} onTouchEnd={this.handleTouchEnd} ref='btn'>{this.props.text||'确认'}</div>
+    );
+  }
+}
